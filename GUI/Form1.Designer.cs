1439cd4c046c0425962387cf86747086d8a37ce8
abagu <abagu@DESKTOP-7S2Q9S8>
2025-09-17 00:47:55 +0200

    Agregar archivos de proyecto.

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,6 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+    }
+}
---
+++ GUI/Form1.cs
@@ -0,0 +1,32 @@
+using System.Drawing.Text;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(500, 416);
+            Name = "Form1";
+            ResumeLayout(false);
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
+    
+
